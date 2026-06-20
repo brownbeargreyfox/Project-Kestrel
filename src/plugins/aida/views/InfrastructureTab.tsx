f6f912c69062403a8667ec183208f9da2e36736d
brownbeargreyfox <xenogenesis8th@gmail.com>
2026-06-20 11:43:06 -0400

    Add AIDA infrastructure placeholder view

---
+++ src/plugins/aida/views/InfrastructureTab.tsx
@@ -0,0 +1,8 @@
+// src/plugins/aida/views/InfrastructureTab.tsx
+import React from "react";
+
+const InfrastructureTab: React.FC = () => (
+  <div className="p-4">InfrastructureTab placeholder</div>
+);
+
+export default InfrastructureTab;
